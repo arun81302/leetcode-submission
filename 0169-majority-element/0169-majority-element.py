cbe02f6c9218c8f10165de460a5309b177cea4f1
Arun <78600634+arun81302@users.noreply.github.com>
2026-08-23 06:29:41 +0530

    Time: 11 ms (50.35%), Space: 13.6 MB (39.43%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def majorityElement(self, arr):
+        cand=0
+        count=0
+        for i in arr:
+            if count==0:
+                cand=i
+            if i==cand:
+                count+=1
+            else:
+                count-=1
+        totalcount=0
+        for i in arr:
+            if i==cand:
+                totalcount+=1
+        n=len(arr)
+        if totalcount>n/2:
+            return cand
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
